7cd94fc281928efe25d783b6d7e8dc86993e7728
Lenny Capellan <3219530+lencap@users.noreply.github.com>
2019-01-21 21:42:02 -0500

    additional Vagrant file adjustments

---
+++ Vagrantfile
@@ -0,0 +1,45 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "centos7-6-1810-virtualbox"
+
+  vm1_name = "kmaster"
+  vm1_ip   = "10.12.13.2"
+
+  vm2_name = "kminion"
+  vm2_ip   = "10.12.13.3"
+
+  config.vm.define vm1_name do |vm1|
+    vm1.vm.hostname = vm1_name
+    vm1.vm.network "private_network", ip: vm1_ip
+    vm1.vm.provision :shell, :inline => "echo " + vm1_ip + " " + vm1_name + " >> /etc/hosts"
+    vm1.vm.provision :shell, :inline => "echo " + vm2_ip + " " + vm2_name + " >> /etc/hosts"
+    vm1.vm.provider "virtualbox" do |vb|
+      vb.name = vm1_name
+      vb.customize ["modifyvm", :id, "--memory", 4096]
+      vb.customize ["modifyvm", :id, "--cpus", 2]
+      vb.customize ["modifyvm", :id, "--audio", "none"]
+      vb.customize ["modifyvm", :id, "--usb", "off"]
+      vb.customize ["modifyvm", :id, "--cpuexecutioncap", "50"]
+    end
+    vm1.vm.provision :shell, path: "bootstrap.sh"
+  end
+
+  config.vm.define vm2_name do |vm2|
+    vm2.vm.hostname = vm2_name
+    vm2.vm.network "private_network", ip: vm2_ip
+    vm2.vm.provision :shell, :inline => "echo " + vm1_ip + " " + vm1_name + " >> /etc/hosts"
+    vm2.vm.provision :shell, :inline => "echo " + vm2_ip + " " + vm2_name + " >> /etc/hosts"
+    vm2.vm.provider "virtualbox" do |vb|
+      vb.name = vm2_name
+      vb.customize ["modifyvm", :id, "--memory", 4096]
+      vb.customize ["modifyvm", :id, "--cpus", 2]
+      vb.customize ["modifyvm", :id, "--audio", "none"]
+      vb.customize ["modifyvm", :id, "--usb", "off"]
+      vb.customize ["modifyvm", :id, "--cpuexecutioncap", "50"]
+    end
+    vm2.vm.provision :shell, path: "bootstrap.sh"
+  end
+
+end
---
+++ bootstrap.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# bootstrap.sh
+
+# Adapted from https://8gwifi.org/docs/kube-install.jsp
+
+# |-------------------------------------------------|----------|----------|
+# | Requirement                                     | kmaster  | kminion  |
+# |-------------------------------------------------|----------|----------|
+# | Disable system swap and SELinux                 | Y        | Y        |
+# | remove any swap entry from /etc/fstab           | Y        | Y        |
+# | net.bridge.bridge-nf-call-iptables is set to 1  | Y        | Y        |
+# | Install Docker & enable on restart              | Y        | Y        |
+# | Install kubeadm                                 | Y        | Y        |
+# | Install kubelet                                 | Y        | N        |
+# | Install kubectl                                 | Y        | N        |
+# | Configure docker cgroupsfs                      | Y        | N        |
+# | Creating Network Addons (flannel/Calico)        | Y        | N        |
+# |-------------------------------------------------|----------|----------|
+
+sudo swapoff -a
+
+sudo setenforce 0
+
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+sudo yum -y update
+
+sudo yum install -y docker 
+
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+sudo yum install -y kubelet kubeadm kubectl
+
+sudo systemctl enable kubelet && sudo systemctl start kubelet
+
+echo "=== docker info | grep -i cgroup ====================="
+sudo docker info | grep -i cgroup
+echo "======================================================"
+
+echo "****** WARNING *****"
+echo "If output of above command isn't: Cgroup Driver: cgroupfs"
+echo "Ensure"
+echo "cat /etc/systemd/system/kubelet.service.d/10-kubeadm.conf"
+echo "[Service]"
+echo "Environment=\"KUBELET_CGROUP_ARGS=--cgroup-driver=cgroupfs\""
+echo "****** WARNING *****"
+echo ""
+
+sudo systemctl daemon-reload && sudo systemctl restart kubelet && sudo kubeadm reset -f 
+
