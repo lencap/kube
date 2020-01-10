556b103ae364d9b2b0af0700c33d401909ea5c2a
Lenny Capellan <3219530+lencap@users.noreply.github.com>
2020-01-10 18:54:28 -0500

    sudo issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@
 # Creating Network Addons (flannel/Calico)         Y    N
 sudo swapoff -a
 sudo setenforce 0
-sudo cat <<EOF >  /etc/sysctl.d/k8s.conf
+sudo bash -c 'cat > /etc/sysctl.d/k8s.conf' << EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
@@ -24,7 +24,7 @@ sudo yum install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl status docker
-sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+sudo bash -c 'cat > /etc/yum.repos.d/kubernetes.repo' << EOF
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
@@ -36,7 +36,7 @@ EOF
 sudo yum install -y kubelet kubeadm kubectl
 sudo systemctl enable kubelet && sudo systemctl start kubelet
 sudo mkdir /etc/systemd/system/kubelet.service.d
-sudo cat <<EOF > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+sudo bash -c 'cat > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf' << EOF
 [Service]
 Environment="KUBELET_CGROUP_ARGS=--cgroup-driver=cgroupfs"
 EOF
