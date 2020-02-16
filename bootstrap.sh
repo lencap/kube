470e57133cb1bb6c660377ab3be261e1b6154639
Lenny Capellan <3219530+lencap@users.noreply.github.com>
2020-02-15 19:54:01 -0500

    adjustments

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 # bootstrap.sh
-
 # Adapted from https://8gwifi.org/docs/kube-install.jsp
-# Requirement                                      k2   k3
-# Update /etc/hosts file                           Y    Y
-# Disable system swap and SELinux                  Y    Y
-# remove any swap entry from /etc/fstab            Y    Y
-# net.bridge.bridge-nf-call-iptables is set to 1   Y    Y
-# Install Docker & enable on restart               Y    Y
-# Install kubeadm                                  Y    Y
-# Install kubelet                                  Y    N
-# Install kubectl                                  Y    N
-# Configure docker cgroupsfs                       Y    N
-# Creating Network Addons (flannel/Calico)         Y    N
+
 sudo bash -c 'cat >> /etc/hosts' << EOF
 10.11.12.2    k2
 10.11.12.3    k3 
 EOF
+
 sudo swapoff -a
 sudo setenforce 0
+
 sudo bash -c 'cat > /etc/sysctl.d/k8s.conf' << EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
+
 sudo sysctl --system
 sudo yum -y update
 sudo yum install -y docker 
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl status docker
+
 sudo bash -c 'cat > /etc/yum.repos.d/kubernetes.repo' << EOF
 [kubernetes]
 name=Kubernetes
@@ -38,14 +31,26 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
-sudo yum install -y kubelet kubeadm kubectl
+
+sudo yum install -y kubelet kubeadm kubectl ipvsadm
 sudo systemctl enable kubelet && sudo systemctl start kubelet
-sudo mkdir /etc/systemd/system/kubelet.service.d
-#sudo bash -c 'cat > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf' << EOF
-#[Service]
-#Environment="KUBELET_CGROUP_ARGS=--cgroup-driver=cgroupfs"
-#EOF
-echo "=== docker info | grep -i cgroup ====================="
-sudo docker info | grep -i cgroup
-echo "======================================================"
-sudo systemctl daemon-reload && sudo systemctl restart kubelet && sudo kubeadm reset -f 
+
+echo "Setting Cgroup Driver"
+CgroupDriver=$(docker info 2>&1 | awk -F': ' '/Cgroup Driver/ {print $2}')
+sudo mkdir -p /etc/systemd/system/kubelet.service.d
+sudo bash -c 'cat > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf' << EOF
+[Service]
+Environment="KUBELET_CGROUP_ARGS=--cgroup-driver=$CgroupDriver"
+EOF
+sudo systemctl daemon-reload && sudo systemctl restart kubelet
+
+echo "Installing calicoctl"
+CalicoCtlURL="https://github.com/projectcalico/calicoctl/releases/download/v3.12.0/calicoctl-linux-amd64"
+sudo curl -L $CalicoCtlURL -o /usr/local/bin/calicoctl
+sudo chmod +x /usr/local/bin/calicoctl
+
+echo "Updating /root/.bashrc"
+sudo bash -c 'echo "export PATH=\$PATH:/usr/local/bin" >> /root/.bashrc'
+sudo bash -c 'echo -e "alias vi=vim\nalias h=history" >> /root/.bashrc'
+
+exit 0
